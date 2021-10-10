99c34ed2eab4b129a04fa7bf3e86c3a52e90bae6
dn3krmn <61933783+dn3krmn@users.noreply.github.com>
2021-10-10 23:16:19 +0300

    Update Main.java

--- coffeShop/src/Main.java
+++ coffeShop/src/Main.java
@@ -13,14 +13,14 @@ public class Main {
 		
 		BaseCustomerManager baseCustomerManager = new  
 				StarbucksCustomerManager(new MernisServiceAdapter());
-		baseCustomerManager.save(new Customer(1, "Döne", "Kahraman", 1998, 
-				"35947874018"));
+		baseCustomerManager.save(new Customer(1, "DÃ¶ne", "Kahraman", 1998, 
+				"12345678901"));
 
 		System.out.println("###########################");
 		System.out.println("NERO");
 		BaseCustomerManager customerManager = new NeroCustomerManager();
-		customerManager.save(new Customer(2, "Döne", "Karaman", 1998, 
-				"35947874018"));
+		customerManager.save(new Customer(2, "DÃ¶ne", "Karaman", 1998, 
+				"12345678901"));
 	}
 
 }
