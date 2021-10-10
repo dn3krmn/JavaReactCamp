95bc3f85f3646dc4faad625f05b3aeffaba721b1
dn3krmn <61933783+dn3krmn@users.noreply.github.com>
2021-10-10 23:21:35 +0300

    Update CustomerManager.java

--- polimorphismDemo/src/polimorphismDemo/CustomerManager.java
+++ polimorphismDemo/src/polimorphismDemo/CustomerManager.java
@@ -5,13 +5,6 @@ public class CustomerManager {
 	private BaseLogger baseLogger;
 	
 	public CustomerManager(BaseLogger logger) {
-		
-		/* bu class newlendiğinde bu constructor çalışır.
-		 * ben bu classı kullanmak istiyorsam bana çalıştığı loggerı söylesin
-		 * baselogger.
-		 * burda aslında base logger değil database logger var
-		 * */
-		
 		this.baseLogger = logger;
 	}
 	
