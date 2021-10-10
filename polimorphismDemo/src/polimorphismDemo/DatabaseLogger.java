960b28e5ab9980d44c332a748088aefc6595591f
dn3krmn <61933783+dn3krmn@users.noreply.github.com>
2021-10-10 23:22:04 +0300

    Update DatabaseLogger.java

--- polimorphismDemo/src/polimorphismDemo/DatabaseLogger.java
+++ polimorphismDemo/src/polimorphismDemo/DatabaseLogger.java
@@ -1,8 +1,6 @@
 package polimorphismDemo;
 
 public class DatabaseLogger extends BaseLogger {
-
-	//override etmek baseloggerda olan metodu burda yeniden oluşturmak
 	
 	public void Log(String message) {
 		System.out.println("Logged to database: " + message);
